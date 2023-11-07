d4ffae63d6798cacb88f3ca764aecfc4013d73f9
Easin2023 <mdeasinarafat016456@gmail.com>
2023-11-08 02:38:07 +0600

    update delete and manage added

--- src/Components/Manage_My_Foods/Manage_My_Foods.jsx
+++ src/Components/Manage_My_Foods/Manage_My_Foods.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import Manage_My_Foods_Card from "../Manage_My_Foods_Card/Manage_My_Foods_Card";
+import {MdModeEditOutline} from 'react-icons/md';
+import {RiDeleteBin6Fill} from 'react-icons/ri';
 
 const Manage_My_Foods = () => {
   const { data } = useQuery({
@@ -16,12 +17,54 @@ const Manage_My_Foods = () => {
         <span className="pl-3">Manage My Foods</span>
       </h1>
       <div>
-          {
-             data?.map(da => <Manage_My_Foods_Card
-             key={da._id}
-             data={da}
-             ></Manage_My_Foods_Card>)  
-          }
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th className="text-xl text-black">Food Name</th>
+                <th className="text-xl text-black">Donator Name</th>
+                <th className="text-xl text-black">Expired_Date_Time</th>
+                <th className="text-xl text-black">operation Button</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((da) => (
+                <tr key={da._id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={da.Food_Image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{da.Food_Name}</div>
+                        <div className="text-sm opacity-50">Quantity {da.Food_Quantity}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {da.Donator_Name}
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                    Location: {da.Pickup_Location}
+                    </span>
+                  </td>
+                  <td>{da.Expired_Date_Time}</td>
+                  <th>
+                    <button className="btn btn-square  mr-2"><RiDeleteBin6Fill  className="text-2xl"></RiDeleteBin6Fill></button>
+                    <button className="btn btn-square mr-3"><MdModeEditOutline className="text-2xl"></MdModeEditOutline></button>
+                    <button className="btn btn-circle ">  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg></button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- src/Components/Manage_My_Foods_Card/Manage_My_Foods_Card.jsx
+++ src/Components/Manage_My_Foods_Card/Manage_My_Foods_Card.jsx
@@ -1,77 +1,61 @@
+import PropTypes from "prop-types";
+
 const Manage_My_Foods_Card = ({ data }) => {
-  // const {
-  //      _id,
-  //      Food_Image,
-  //      Food_Name,
-  //      Donator_Image,
-  //      Donator_Name,
-  //      Food_Quantity,
-  //      Pickup_Location,
-  //      Expired_Date_Time,
-  //      Additional_Notes,
-  //    } = data || {};
+  const {
+    _id,
+    Food_Image,
+    Food_Name,
+    Donator_Image,
+    Donator_Name,
+    Food_Quantity,
+    Pickup_Location,
+    Expired_Date_Time,
+    Additional_Notes,
+  } = data || {};
+  console.log(data);
 
   return (
     <div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Name</th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img
+                  src="/tailwind-css-component-profile-2@56w.png"
+                  alt="Avatar Tailwind CSS Component"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br />
+          <span className="badge badge-ghost badge-sm">
+            Desktop Support Technician
+          </span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
     </div>
   );
+};
+
+Manage_My_Foods_Card.propTypes = {
+  data: PropTypes.object,
 };
 
 export default Manage_My_Foods_Card;
